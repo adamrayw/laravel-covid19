5b31895d5537f531949907c1ade124b3062c7ad6
adamray <adamrayw@users.noreply.github.com>
2021-07-11 20:00:09 +0700

    first commit to heroku

---
+++ resources/views/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layout.template')
+
+
+@section('title', 'Live Data Covid-19 | Home')
+
+@section('content')
+<section>
+    <header class="max-w-4xl mx-auto">
+        <div class="mt-24 md:mt-32 text-center">
+            <h1 class="text-4xl font-light">Kawal Covid-19</h1>
+            <div class="flex justify-center items-center mt-6">
+                <p class="font-extralight">Live Data Covid-19 Indonesia</p>
+                <img class="w-6 h-auto ml-2" src="https://img.icons8.com/color/48/000000/indonesia.png" />
+            </div>
+            <div class="statistik grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-12 mt-10 px-8">
+                @foreach ($data as $datas)
+                <div class="card px-6 py-4 border-b-4 border-white bg-red-500 text-white rounded-md shadow-lg">
+                    <p class="font-light">TOTAL POSITIF</p>
+                    <div class="flex items-baseline">
+                        <p class="text-2xl font-bold">{{ $datas['positif'] }}</p>
+                        <p class="text-light text-xs ml-1">ORANG</p>
+                    </div>
+                </div>
+                <div class="card px-6 py-4 border-b-4 border-white bg-green-500 text-white rounded-md shadow-lg">
+                    <p class="font-light">TOTAL SEMBUH</p>
+                    <p class="text-2xl font-bold">{{ $datas['sembuh'] }}</p>
+                </div>
+                <div class="card px-6 py-4 border-b-4 border-white bg-gray-500 text-white rounded-md shadow-lg">
+                    <p class="font-light">TOTAL MENINGGAL</p>
+                    <p class="text-2xl font-bold">{{ $datas['meninggal'] }}</p>
+                </div>
+                @endforeach
+            </div>
+            <div class="lastupdate text-center">
+                @foreach ($dataupdate as $date)
+                <p>{{ $date['attributes']['Last_Update'] }}</p>
+                @endforeach
+            </div>
+    </header>
+</section>
+@endSection
---
+++ resources/views/layout/template.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
+    <title>Covid-19 Report</title>
+</head>
+
+<body>
+    <section class="nav">
+        <div class="navbar-shadow fixed top-0 z-20 w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800 shadow-md">
+            <div x-data="{ open: false }" class="flex flex-col px-4 mx-auto md:items-center md:justify-between md:flex-row lg:px-8">
+                <div class="flex flex-row items-center justify-between py-4">
+                    <a href="index" class="text-2xl px-0 md:px-4 font-light tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline">covid-19</a>
+                    <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
+                        <svg fill="currentColor" viewBox="0 0 20 20" class="w-6 h-6">
+                            <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                            <path x-show="open" fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+                <nav :class="{'flex': open, 'hidden': !open}" class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row">
+                    <a class="md:px-4 py-2 mt-2 text-lg font-light text-gray-900 rounded-lg dark-mode:bg-gray-700 md:mt-0 hover:text-gray-500" href="all-course">All Course</a>
+                    <a class="md:px-4 py-2 mt-2 text-lg font-light bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-500 " href="contact">Contact</a>
+                    <a class="md:px-4 py-2 mt-2 text-lg font-light bg-transparent rounded-lg md:mt-0 md:ml-4 hover:text-gray-500  " href="roadmap">Alur Belajar</a>
+                </nav>
+            </div>
+        </div>
+    </section>
+    @yield('content')
+</body>
+
+</html>
